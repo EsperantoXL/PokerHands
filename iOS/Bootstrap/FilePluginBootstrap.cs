fba90c16feb89a6e1edd051a1181defebfa32b52
Ewoud Stuurman <e.stuurman@esperantoxl.nl>
2016-03-02 15:09:59 +0100

    Added file and download plugin

---
+++ iOS/Bootstrap/FilePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace PokerHands.iOS.Bootstrap
+{
+    public class FilePluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.File.PluginLoader, MvvmCross.Plugins.File.iOS.Plugin>
+    {
+    }
+}
