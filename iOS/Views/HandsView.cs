8f6acd84646d45058eabe84d13285fc7af328baa
Ewoud Stuurman <e.stuurman@esperantoxl.nl>
2016-03-02 13:34:31 +0100

    Added a hands view for iOS

---
+++ iOS/Views/HandsView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using UIKit;
+using MvvmCross.iOS.Views;
+
+namespace PokerHands.iOS
+{
+    //Do we still need a mvx view controller
+    public partial class HandsView : MvxViewController
+    {
+        public HandsView()
+            : base("HandsView", null)
+        {
+        }
+
+        //we migth want to do some things with a table here
+    }
+}
+
+
