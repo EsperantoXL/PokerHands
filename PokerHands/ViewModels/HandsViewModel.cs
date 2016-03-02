570ca38347f06f46919606b7cb25fab63c48bb6d
Ewoud Stuurman <e.stuurman@esperantoxl.nl>
2016-03-02 13:31:41 +0100

    We want to have a list of hands so we need a Hands View Model

---
+++ PokerHands/ViewModels/HandsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+
+namespace PokerHands.Core
+{
+    /// <summary>
+    /// ViewModel that shows the possible hands and the ranking order
+    /// </summary>
+    public class HandsViewModel : MvxViewModel
+    {
+        //we need to setup a constructor 
+        //with a nice service to provide the hands
+        public HandsViewModel()
+        {
+        }
+
+        //We need a collection of hand objects
+
+        //We need to capture the moment a hand is pressed
+    }
+}
