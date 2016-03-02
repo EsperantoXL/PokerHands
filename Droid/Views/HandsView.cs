8bc9bc713381e5bfdd25fe857907488b4e43bb52
Ewoud Stuurman <e.stuurman@esperantoxl.nl>
2016-03-02 13:37:03 +0100

    Add hands view for android

---
+++ Droid/Views/HandsView.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+
+namespace PokerHands.Droid
+{
+    [Activity(Label = "HandsView")]			
+    public class HandsView : MvxActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            // for sure we will create our application 
+            // this will include a nice table
+            SetContentView(Resource.Layout.HandsView);
+        }
+    }
+}
+
